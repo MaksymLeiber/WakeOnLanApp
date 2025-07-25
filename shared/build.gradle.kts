d85f2b1c934a861f6129fe7c3912b0c6bf910b6d
maksym <pro.leuber@gmail.com>
2025-07-25 13:20:21 +0300

    feat: enable cocoapods and proper KMP plugins for XCFramework build

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,64 +1,39 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("org.jetbrains.kotlin.native.cocoapods")
 }
 
 kotlin {
-
-    // Target declarations - add or remove as needed below. These define
-    // which platforms this KMP module supports.
-    // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
-    android()
-
-    // For iOS targets, this is also where you should
-    // configure native binary output. For more information, see:
-    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
-
-    // A step-by-step guide on how to include this library in an XCode
-    // project can be found here:
-    // https://developer.android.com/kotlin/multiplatform/migrate
-    val xcfName = "sharedKit"
+    androidTarget()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     cocoapods {
         summary = "WakeOnLanApp shared module"
         homepage = "https://github.com/MaksymLeiber/WakeOnLanApp"
         ios.deploymentTarget = "14.1"
         framework {
-            baseName = xcfName
+            baseName = "sharedKit"
         }
     }
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    // Source set declarations.
-    // Declaring a target automatically creates a source set with the same name. By default, the
-    // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
-    // common to share sources between related targets.
-    // See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
     sourceSets {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
             }
         }
-
         commonTest {
             dependencies {
                 implementation(libs.kotlin.test)
             }
         }
-
         androidMain {
             dependencies {
-                // Add Android-specific dependencies here. Note that this source set depends on
-                // commonMain by default and will correctly pull the Android artifacts of any KMP
-                // dependencies declared in commonMain.
             }
         }
-
         getByName("androidDeviceTest") {
             dependencies {
                 implementation(libs.androidx.runner)
@@ -66,16 +41,9 @@ kotlin {
                 implementation(libs.androidx.junit)
             }
         }
-
         iosMain {
             dependencies {
-                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
-                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
-                // part of KMP’s default source set hierarchy. Note that this source set depends
-                // on common by default and will correctly pull the iOS artifacts of any
-                // KMP dependencies declared in commonMain.
             }
         }
     }
-
 }
