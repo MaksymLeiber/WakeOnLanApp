25e41e9dbe471d6651976990712c0793d70dd5c8
maksym <pro.leuber@gmail.com>
2025-07-25 13:45:45 +0300

    fix: add android block with compileSdk, namespace, minSdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
 }
